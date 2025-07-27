b76d6c67c2c0119b6094b6ee8ee606ed115bb1bb
Clara Miño <claraa.mino12@gmail.com>
2025-07-27 11:55:55 -0300

    estructura

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import TituloPrincipal from "./components/TituloPrincipal/TituloPrincipal";
+
+function App() {
+
+  return(
+    <>
+    
+    </>
+  )
+
+}
+
+export default App
