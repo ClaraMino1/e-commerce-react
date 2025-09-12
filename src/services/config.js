4e94024fdd5567cee8b502122c45453655077981
Clara Miño <claraa.mino12@gmail.com>
2025-09-12 14:20:00 -0300

    variables de entorno

--- src/services/config.js
+++ src/services/config.js
@@ -3,11 +3,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "thelirium-b2010.firebaseapp.com",
-  projectId: "thelirium-b2010",
-  storageBucket: "thelirium-b2010.firebasestorage.app",
-  messagingSenderId: "805221309383",
-  appId: "1:805221309383:web:86e27e32e58e5a38e317e2"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
